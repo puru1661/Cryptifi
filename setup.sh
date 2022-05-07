44cdbd412a3f73db49e27a46d80afc6fd6726ec5
Purushottam <purushottamd.16@gmail.com>
2022-05-07 10:16:21 +0400

    init

---
+++ setup.sh
@@ -0,0 +1,25 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Monaco;}
+{\colortbl;\red255\green255\blue255;\red238\green252\blue122;\red30\green31\blue41;\red246\green246\blue239;
+\red124\green228\blue252;\red72\green255\blue104;}
+{\*\expandedcolortbl;;\cssrgb\c94510\c98039\c54902;\cssrgb\c15686\c16471\c21176;\cssrgb\c97255\c97255\c94902;
+\cssrgb\c54510\c91373\c99216;\cssrgb\c31373\c98039\c48235;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs28 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 mkdir\cf4 \strokec4  -p ~/.streamlit/\
+\
+\pard\pardeftab720\partightenfactor0
+\cf5 \strokec5 echo\cf4 \strokec4  \cf6 \strokec6 "\\\
+[general]\\n\\\
+email = \\"your-email@domain.com\\"\\n\\\
+"\cf4 \strokec4  > ~/.streamlit/credentials.toml\
+\
+\cf5 \strokec5 echo\cf4 \strokec4  \cf6 \strokec6 "\\\
+[server]\\n\\\
+headless = true\\n\\\
+enableCORS=false\\n\\\
+port = \cf4 \strokec4 $PORT\cf6 \strokec6 \\n\\\
+"\cf4 \strokec4  > ~/.streamlit/config.toml}
