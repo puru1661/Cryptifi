f5aaf93ecf6d57dfd9d5a453222d991a0f0196d5
Purushottam <purushottamd.16@gmail.com>
2022-05-07 12:16:34 +0400

    update req.txt

--- setup.sh
+++ setup.sh
@@ -1,11 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "
-[general]n
-email = "purushottamd.16@gmail"n
-" > ~/.streamlit/credentials.toml
-echo "
 [server]n
 headless = truen
 enableCORS=falsen
 port = $PORTn
-" > ~/.streamlit/config.toml
+" 
